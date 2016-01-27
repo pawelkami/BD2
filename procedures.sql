b761f557e5429c4c203ed1023b18803c95628a37
jmuczyns <jmuczyns@mion.elka.pw.edu.pl>
2016-01-27 21:59:01 +0100

    added view and therefore change procedure wypiszpracownikowplacowek( integer) :heart:

--- procedures.sql
+++ procedures.sql
@@ -15,14 +15,11 @@ RETURNS TABLE
 $func$
 BEGIN
 		RETURN QUERY
-		SELECT P.imie, P.nazwisko, P."PESEL", P."id_PLACOWKA", P."login",
-		D."miasto", D."ulica", D."numer domu", D."numer lokalu", D."telefon"
-		FROM "PRACOWNIK" AS P JOIN "DANE KONTAKTOWE" AS D ON D."id" = P."id_DANE KONTAKTOWE"
-		WHERE P."id_PLACOWKA" = x;
+		SELECT * FROM "PRACOWNICY PLACOWEK"
+		WHERE "PRACOWNICY PLACOWEK"."id_PLACOWKA" = x;
 END
 $func$
 LANGUAGE plpgsql;
-
 
 
 CREATE OR REPLACE FUNCTION  sredniaIloscWypozyczen(mm int, fromYear int, toYear int) 
