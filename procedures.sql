b22359a76b4ac482e4c4e0497a2e24af4390a6fe
Jakub Guzek <j.guzek@stud.elka.pw.edu.pl>
2016-01-27 03:10:40 +0100

    Add procedures (and needed type czesc_type)

--- procedures.sql
+++ procedures.sql
@@ -23,3 +23,43 @@ END
 $func$
 LANGUAGE plpgsql;
 
+
+
+CREATE OR REPLACE FUNCTION  sredniaIloscWypozyczen(mm int, fromYear int, toYear int) 
+RETURNS int AS
+$func$
+declare
+srednia int;
+BEGIN
+	SELECT COUNT(*) into srednia 
+	FROM "WYPOZYCZENIE"
+	WHERE mm = Extract(month from "data_wypozyczenia")
+		AND fromYear <= Extract(year from "data_wypozyczenia")
+		AND toYear >= Extract(year from "data_wypozyczenia");
+	RETURN srednia;
+END
+$func$
+LANGUAGE plpgsql;
+
+
+create type czesc_type as (rodzaj character varying(32), info character varying(50), ilosc integer);
+
+CREATE OR REPLACE FUNCTION  stanMagazynowy(placowka int) 
+RETURNS SETOF czesc_type AS
+$func$
+declare
+r czesc_type%rowtype;
+BEGIN
+	FOR r IN 	
+		SELECT RCE."rodzaj", CE."info", CE."ilosc"
+		FROM "PLACOWKA" AS P, "SERWIS MAGAZYN" AS SM, "SERWIS_MAGAZYN_CZESC_EKSPLOATACYJNA" AS SMCE,
+		 "CZESC EKSPLOATACYJNA" AS CE,"RODZAJ CZESCI EKSPLOATACYJNEJ" AS RCE
+		WHERE P."id" = placowka AND SM."id_PLACOWKA" = P."id" AND SM."id" = SMCE."id_SERWIS MAGAZYN" AND SMCE."id_CZESC EKSPLOATACYJNA" = CE."id"
+		 AND CE."id_RODZAJ CZESCI EKSPLOATACYJNEJ" = RCE."id"
+	LOOP
+		RETURN NEXT r;
+	END LOOP;
+END
+$func$
+LANGUAGE plpgsql;
+
