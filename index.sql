2d6d2579b45c6d79e5fb664dadf0930186d15802
jmuczyns <jmuczyns@mion.elka.pw.edu.pl>
2016-01-28 23:57:42 +0100

    add new indexes :poop:

--- index.sql
+++ index.sql
@@ -12,3 +12,48 @@ CREATE INDEX idxrocznik
   ON public."SAMOCHOD"
   USING hash
   (rocznik);
+
+CREATE INDEX idxrodzaj
+  ON public."SAMOCHOD"
+  USING hash
+  (rodzaj);
+
+CREATE INDEX idxskrzynia
+  ON public."SAMOCHOD"
+  USING btree
+  (automatyczna_skrzynia);
+
+CREATE INDEX idxpojemnosc
+  ON public."SAMOCHOD"
+  USING btree
+  (pojemnosc_silnika COLLATE pg_catalog."default");
+
+CREATE INDEX idxmarka
+  ON public."SAMOCHOD"
+  USING hash
+  (marka COLLATE pg_catalog."default");
+
+CREATE INDEX idxmodel
+  ON public."SAMOCHOD"
+  USING hash
+  (model COLLATE pg_catalog."default");
+
+CREATE INDEX idxprzeglad
+  ON public."SAMOCHOD"
+  USING hash
+  (data_przegladu);
+
+CREATE INDEX idxcena
+  ON public."SAMOCHOD"
+  USING hash
+  (cena_za_dzien);
+
+CREATE INDEX idxczy_wypozyczony
+  ON public."SAMOCHOD"
+  USING hash
+  (czy_wypozyczony);
+
+CREATE INDEX idxwartosc
+  ON public."SAMOCHOD"
+  USING hash
+  (wartosc);
