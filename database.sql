a2afdaf1a3c6486902893ccd45f761ecfd583d34
tom3097 <T.Bochenski@stud.elka.pw.edu.pl>
2016-01-28 22:14:51 +0100

    pop reseta

--- database.sql
+++ database.sql
@@ -958,6 +958,7 @@ ON DELETE SET NULL ON UPDATE CASCADE;
 --1.
 ALTER TABLE "WYPOZYCZENIE" ADD COLUMN czas_wypozyczenia interval;
 
+
 CREATE OR REPLACE FUNCTION set_czas_wypozyczenia() 
 RETURNS TRIGGER 
 AS $$
@@ -971,8 +972,10 @@ CREATE TRIGGER set_czas_wypozyczenia_wypozyczenie
 BEFORE INSERT OR UPDATE ON "WYPOZYCZENIE"
 FOR EACH ROW 
   EXECUTE PROCEDURE set_czas_wypozyczenia();
-  
+ 
+ 
 ALTER TABLE "KLIENT INDYWIDUALNY" ADD COLUMN czy_wypozyczenie_aktywne boolean;
+
 
 CREATE OR REPLACE FUNCTION reset_czy_wypozyczenie_aktywne()
 RETURNS TRIGGER
@@ -981,6 +984,7 @@ BEGIN
   NEW.czy_wypozyczenie_aktywne := FALSE;
   RETURN NEW;
 END $$ LANGUAGE plpgsql;
+
 
 CREATE TRIGGER reset_czy_wypozyczenie_aktywne_klient_indywidualny
 BEFORE INSERT ON "KLIENT INDYWIDUALNY"
@@ -996,6 +1000,29 @@ BEGIN
   WHERE "id" = NEW."id_KLIENT INDYWIDUALNY";
   RETURN NEW;
 END $$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION set_czy_wypozyczenie_aktywne()
+RETURNS TRIGGER
+AS $$
+DECLARE
+myid boolean;
+czas timestamp;
+BEGIN
+  SELECT "czy_wypozyczenie_aktywne" INTO myid FROM "KLIENT INDYWIDUALNY" WHERE id = NEW."id_KLIENT INDYWIDUALNY";
+  IF myid = TRUE THEN
+    SELECT "WYPOZYCZENIE"."data_zwrotu" INTO czas FROM "WYPOZYCZENIE" WHERE "WYPOZYCZENIE"."id_KLIENT INDYWIDUALNY" = NEW."id_KLINET INDYWIDUALNY";
+	IF now() < czas THEN
+      RAISE EXCEPTION 'Klient indywidualny jest juz w posiadaniu samochodu.';
+	END IF;
+  END IF;
+  IF now() < NEW."data_zwrotu" THEN
+    UPDATE "KLIENT INDYWIDUALNY" SET czy_wypozyczenie_aktywne = TRUE
+	WHERE "id" = NEW."id_KLIENT INDYWIDUALNY";
+  END IF;
+  RETURN NEW;
+END $$ LANGUAGE plpgsql;
+
 
 CREATE TRIGGER set_czy_wypozyczenie_aktywne_klient_indywidualny
 BEFORE INSERT ON "WYPOZYCZENIE"
